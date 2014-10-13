8c66525549d92c8a583c8e9cd9074e6fe8d91cca
DigiKicki <DigiKicki@users.noreply.github.com>
2014-10-13 17:38:45 +0200

    Code format

--- ArduinoGoal/ArduinoGoal.ino
+++ ArduinoGoal/ArduinoGoal.ino
@@ -7,30 +7,32 @@ int do_debug=1;
 
 void setup()
 {
-if (do_debug ==1) Serial.begin(9600);
-//benötigte Ports aktivieren
-pinMode(LEDAktiv, OUTPUT);
-pinMode(LEDInaktiv, OUTPUT); 
-pinMode(LEDIr, OUTPUT);
-pinMode(PD, INPUT);
-//IR Led einschalten
-digitalWrite(LEDIr,HIGH); //einschalten der IR Diode
+  if (do_debug ==1) Serial.begin(9600);
+  //benötigte Ports aktivieren
+  pinMode(LEDAktiv, OUTPUT);
+  pinMode(LEDInaktiv, OUTPUT); 
+  pinMode(LEDIr, OUTPUT);
+  pinMode(PD, INPUT);
+  //IR Led einschalten
+  digitalWrite(LEDIr,HIGH); //einschalten der IR Diode
 }
 
 void loop()
 {
-val=analogRead(PD); //Photodiode auslesen
-if (val<30) //Wird der Lichtstrahl unterbrochen...
-{
-digitalWrite(LEDAktiv, HIGH);
-digitalWrite(LEDInaktiv, LOW);
-} 
-else
-{
-digitalWrite(LEDAktiv, LOW);
-digitalWrite(LEDInaktiv, HIGH);
-}
-if (do_debug ==1) 
-  {  Serial.println(val); //Werte zum Serial Monitor senden
-  delay(500);  } 
+  val=analogRead(PD); //Photodiode auslesen
+  if (val<30) //Wird der Lichtstrahl unterbrochen...
+  {
+    digitalWrite(LEDAktiv, HIGH);
+    digitalWrite(LEDInaktiv, LOW);
+  } 
+  else
+  {
+    digitalWrite(LEDAktiv, LOW);
+    digitalWrite(LEDInaktiv, HIGH);
+  }
+  if (do_debug ==1) 
+  {  
+    Serial.println(val); //Werte zum Serial Monitor senden
+    delay(500);  
+  } 
 } 
