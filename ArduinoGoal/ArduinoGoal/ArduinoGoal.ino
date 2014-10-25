3d8a1554831661adaa7e0bd721f18a421460d772
DigiKicki <DigiKicki@users.noreply.github.com>
2014-10-25 18:42:08 +0200

    umgestellt auf _BV

--- ArduinoGoal/ArduinoGoal/ArduinoGoal.ino
+++ ArduinoGoal/ArduinoGoal/ArduinoGoal.ino
@@ -4,20 +4,17 @@ int PhotoDiode=0;
 //Analoger Eingang der Photodiode
 int value=0; //Messdaten
 int grenzwert = 20;
+volatile boolean interrupt;
 
 void setup()
 {
   Serial.begin(9600);
   
   // Set ACSR Register for analog interrupt
-  ACSR = 
-  (0<<ACD) |   // Analog Comparator: Enabled
-  (0<<ACBG) |   // Analog Comparator Bandgap Select: AIN0 is applied to the positive input
-  (0<<ACO) |   // Analog Comparator Output: Off
-  (1<<ACI) |   // Analog Comparator Interrupt Flag: Clear Pending Interrupt
-  (1<<ACIE) |   // Analog Comparator Interrupt: Enabled
-  (0<<ACIC) |   // Analog Comparator Input Capture: Disabled
-  (1<<ACIS1) | (1<ACIS0);   // Analog Comparator Interrupt Mode: Comparator Interrupt on Rising Output Edge
+  ACSR =  _BV (ACBG)    // set internal bandgap (1.1V)
+        | _BV (ACI)     // (Clear) Analog Comparator Interrupt Flag
+        | _BV (ACIE)    // Analog Comparator Interrupt Enable
+        | _BV (ACIS1);  // ACIS1, ACIS0: Analog Comparator Interrupt Mode Select (trigger on falling edge)
   
   //benötigte Ports aktivieren
   pinMode(LEDAktiv, OUTPUT);
@@ -31,22 +28,13 @@ void setup()
 
 void loop()
 {
-
+  if (interrupt) {
+    interrupt = false;
+    Serial.println("TOOOOOOOOOOOOOOOOOOOOOOR");
+  }
 }
 
 ISR(ANALOG_COMP_vect)
 {
-  value=analogRead(PhotoDiode); //Photodiode auslesen
-  Serial.println(value);
-  if (value<grenzwert) //Wird der Lichtstrahl unterbrochen...
-  {
-    digitalWrite(LEDAktiv, HIGH);
-    digitalWrite(LEDInaktiv, LOW);
-    Serial.println("TOOOOOOOOOOOOOOOOOOOOOOR");
-  }
-  else
-  {
-    digitalWrite(LEDAktiv, LOW);
-    digitalWrite(LEDInaktiv, HIGH);
-  }
+  interrupt=true;
 }
