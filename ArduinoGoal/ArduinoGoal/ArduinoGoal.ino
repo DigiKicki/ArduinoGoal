c51672d84213daa78d7499498de9218b5d70ed3b
DigiKicki <DigiKicki@users.noreply.github.com>
2014-11-15 17:43:12 +0100

    changed to polling

--- ArduinoGoal/ArduinoGoal/ArduinoGoal.ino
+++ ArduinoGoal/ArduinoGoal/ArduinoGoal.ino
@@ -1,91 +1,58 @@
 #include <analogComp.h>
 const int DIODE_PIN_1 = 0;
 const int DIODE_PIN_2 = 1;
-const int LED_PIN_1 = 9;
-const int LED_PIN_2 = 10;
-const int PWM_START_VAL = 255;
+const int MAX_STEPS = 5;
 
-int PhotoDiode=0;
-
-//int value=0; //Messdaten
-//int grenzwert = 20;
-volatile boolean goal;
-boolean mutex = false;
-long time = 0;
-int pwm = 0;
-int minvalue = 9999;
+int currentDiodeVal;
+int beforeDiodeVal_1;
+int beforeDiodeVal_2;
+int stepCounter_1;
+int stepCounter_2;
 
 void setup()
 {
   Serial.begin(9600);
   
-  //analogWrite(LED_PIN_1, PWM_START_VAL);
-  //analogWrite(LED_PIN_2, PWM_START_VAL);
-  delay(50);
-  //calibrateLedPwm();
-  // AIN0 PIN6
-  // AIN1 Pin7
-  //380mV - 100mV
-  // pwm -> 220mV
-  //Timer 2 pwm pins: 3,11
-  // prescaler auf min
-  //TCCR2B = TCCR2B & 0b11111000 | _BV(CS20);
+  pinMode(DIODE_PIN_1, INPUT);
+  pinMode(DIODE_PIN_2, INPUT);
   
-  
-  
-  pinMode(PhotoDiode, INPUT);
-
-  
-  //benötigte Ports aktivieren
-  //pinMode(LEDAktiv, OUTPUT);
-  
-  //pinMode(LEDInaktiv, OUTPUT);
-  //pinMode(LEDIr, OUTPUT);
-  
-  // enable analog comparator using reference voltage on PIN6 and analog pin 0 for input voltage
-  analogComparator.setOn(AIN0, 1);
-  analogComparator.enableInterrupt(ISR_goalDetected, FALLING);
-  //IR Led einschalten
-  delay(50);
-  int v = analogRead(0);
-  Serial.println(v);
-  //digitalWrite(LEDIr,HIGH); //einschalten der IR Diode
-  //int val = (6.08*diode*1000/1023-25)/4.4;
-  //Serial.println(val);
-  //analogWrite(3, val);
-  //1136mV
-  //20mV
-  //dV=1120
-  // step = 4.4mV
-
-  
-  time = millis();
+  //beforeDiodeVal_1 = analogRead(DIODE_PIN_1);
+  //beforeDiodeVal_2 = analogRead(DIODE_PIN_2);
 }
 
 
 
 void loop()
 {
-  if (goal && !mutex) {
-  int val = analogRead(0);
-  if (val < minvalue ) {
-    minvalue = val;
-    Serial.println(minvalue);
-  }
-    Serial.println("TOOOOOOOOOOOOOOOOOOOOOOR");
-    time = millis();
-    mutex = true;
-  }
-  if ((millis()-time) > 3000) {
-    goal = mutex = false;
-  }
+  checkDiodeValue(DIODE_PIN_1, &beforeDiodeVal_1, &stepCounter_1);
+  checkDiodeValue(DIODE_PIN_2, &beforeDiodeVal_2, &stepCounter_2);
 }
 
 void serialEvent() {
-  Serial.read();
-  minvalue = 9999;
+
 }
 
-void ISR_goalDetected() {
-  goal = true;
+void checkDiodeValue(int pin, int* beforeDiodeVal, int* counter) {
+  currentDiodeVal = analogRead(pin);
+  if (currentDiodeVal <= *beforeDiodeVal) {
+    *counter++;
+  } else {
+    *counter = 0;
+  }
+  *beforeDiodeVal = currentDiodeVal;
+  
+  if (*counter >= MAX_STEPS) {
+    *counter = 0;
+    goalDetected(pin);
+  }
+}
+
+void goalDetected(int pin) {
+  if (pin == DIODE_PIN_1) {
+    //goal for team yellow
+    Serial.println("TOOOOOOOOOOR_1");
+  } else {
+    //goal for team blue
+    Serial.println("TOOOOOOOOOOR_2");
+  }
 }
