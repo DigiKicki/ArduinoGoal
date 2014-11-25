0ef24f76e4f68ef440eaf7ff3b5ad74d6e55ef98
DigiKicki <DigiKicki@users.noreply.github.com>
2014-11-25 14:01:04 +0100

    TEST software i2c and interrupt

--- ArduinoGoal/_7SegDisplay/_7SegDisplay.ino
+++ ArduinoGoal/_7SegDisplay/_7SegDisplay.ino
@@ -1,18 +1,28 @@
-#include <Adafruit_GFX.h>
+
+
+//#include <Adafruit_GFX.h>
 
 #include <Wire.h>
-#include <Adafruit_LEDBackpack.h>
+//#include <Adafruit_LEDBackpack.h>
 
-Adafruit_7segment clockDisplay = Adafruit_7segment();
+#define SCL_PIN 2 
+#define SCL_PORT PORTD 
+#define SDA_PIN 0 
+#define SDA_PORT PORTC 
+//#include <SoftI2C.h>
+#include <SoftI2CMaster.h>
+
+//Adafruit_7segment clockDisplay;// = Adafruit_7segment();
 int i = 0;
 volatile boolean second = false;
+int MatchTime, num;
 
 void setup() {
   Serial.begin(9600);
   //clockDisplay 
-  clockDisplay.begin(0x71);
+  //clockDisplay.begin(0x71);
   
-  TCCR1B=0;
+  /*TCCR1B=0;
   TCCR1A=0;
   TCNT1=0;
   
@@ -21,8 +31,32 @@ void setup() {
   TCCR1B |= _BV(WGM12); // CTC mode
   TCCR1B |= _BV(CS12); // 256 prescaler
   TIMSK1 |= _BV(OCIE1A); // enable timer compare interrupt
+  */
+  //interrupts();
   
-  interrupts();
+  if (!i2c_init()) {
+    Serial.println("init");
+  }
+  if (!i2c_start(0x71 | I2C_WRITE)) {
+    Serial.println("start");
+  }
+  byte val = 1;
+  if (!i2c_write(val)) {
+    Serial.println("write");
+  }
+  i2c_stop();
+}
+
+/*void WriteTimeOnDisplay()
+{
+  int TimerValue=MatchTime;
+  int TimerValueSec = TimerValue/1000;
+  int DisplayTimeMin = TimerValueSec/60;
+  int DisplayTimeSec = TimerValueSec-(DisplayTimeMin*60);
+   
+  clockDisplay.print(num);
+  clockDisplay.drawColon(true);
+  clockDisplay.writeDisplay();
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -38,14 +72,18 @@ ISR(TIMER1_COMPA_vect)
   
   //Serial.println("Sekunde");
   //interrupts();
-}
+}*/
 
 void loop() {
+  delay(1000);
+  
+  
+  
   //delay(1000);
-  if (second) {
-    clockDisplay.print(i++);
-    clockDisplay.drawColon(true);
-    clockDisplay.writeDisplay();
+  if (second && false) {
+    //clockDisplay.print(i++);
+    //clockDisplay.drawColon(true);
+    //clockDisplay.writeDisplay();
     second = false;
   }
   //Serial.println("asd");
