bcdcc2bc56935f3dc81b54155274537339c4a509
DigiKicki <DigiKicki@users.noreply.github.com>
2015-04-07 11:03:36 +0200

    START- und RESTART-Bytes wurden verschoben

--- ArduinoGoal/libraries/SerialCommunication/SerialCommunication.h
+++ ArduinoGoal/libraries/SerialCommunication/SerialCommunication.h
@@ -5,9 +5,9 @@
 #define SERIAL_GOAL_UNO 0x3C		// next byte: minute, next byte second
 #define SERIAL_TIME_OVER 0x3D
 #define SERIAL_GOAL_ANSWER 0x41		// next byte: minute, next byte second
+#define SERIAL_GAME_START 0x3F
+#define SERIAL_GAME_RESET 0x40
 
 // Bytes send from MEGA to UNO
 #define SERIAL_GOAL_MEGA 0x3E		// answer byte: minute, next byte second
-#define SERIAL_GAME_START 0x3F
-#define SERIAL_GAME_RESET 0x40
 //#define SERIAL_TIME_CONFIG 0x42	// next byte: minute (if minute is 0 then use no time -> goal display)
